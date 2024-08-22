7556349321e50cea57f4af59249a370fe7c5289d
Ubuntu <ubuntu@ip-172-31-59-181.ec2.internal>
2024-08-22 13:53:32 +0000

    c1

---
+++ script1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Author:
+#Description:
+#Date:
+echo "hi,i'm interested in the aws shell scripting course.can you provide some details?"
+read
+echo "how long is the course?"
+read
+echo "what are the timings?"
+read
+echo "Do i need any prior knowledge to join?"
+read
+echo "How can i register for the course?"
+read
